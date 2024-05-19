bcc4fc87a0d72e057860808ebbf0b1fd3221f373
Mindohyeon <min50875@naver.com>
2024-05-19 21:17:36 +0900

    init

---
+++ MG-Order/MG_OrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MG_OrderApp.swift
+//  MG-Order
+//
+//  Created by 민도현 on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MG_OrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
